ca82e0880013d3ce78cefcb774358b232810e283
unknown <baruaofficials@gmail.comm>
2022-05-10 12:58:55 +0600

    add

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
-// 8fTlYJgdMZBFzQoJ
+
 
 
 // middleware
@@ -15,13 +15,95 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.w3hjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w3hjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db("carnation").collection("inventory");
+
+
+
+        // get data from database****************
+
+        app.get('/inventory', async (req, res) => {
+
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const inventory = await cursor.toArray();
+            res.send(inventory);
+        });
+
+
+        // get data from database****************
+
+
+
+
+
+        // show data from database****************
+
+
+
+        app.get('/inventory/:id', async (req, res) => {
+
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const inventory = await itemsCollection.findOne(query);
+            res.send(inventory);
+        });
+
+
+
+
+        // show data from database****************
+
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
